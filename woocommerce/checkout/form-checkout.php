99005ecedc91978f6f22f3daad442a8edf687672
Esteban Ramirez <estebanramirezcc@gmail.com>
2026-03-09 08:05:07 -0500

    fixes

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -1,5 +1,5 @@
 <!-- Hardcoded background black as a temporary fix -->
-<div class="flex flex-col" style="background-color: #000; min-height: 100vh;">
+<div class="max-w-screen-xxl m-auto py-16 flex-grow text-white" style="background-color: #000;">
 <?php
 /**
  * Checkout Form
